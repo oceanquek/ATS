c360dc139c8334f9cf50b94281570653bff39d32
ZY <zhangyi.201092@gmail.com>
2021-05-04 21:46:04 +0800

    ZY lab  - Spring boot

---
+++ ZY/labs/ATSProj-spring/ATSProj_Spring/src/main/java/com/domain/repository/CountryRepository.java
@@ -0,0 +1,9 @@
+package com.domain.repository;
+
+import com.domain.entity.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Integer> {
+}
