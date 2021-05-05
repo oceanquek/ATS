3e3a28d6eb470d1ac97bd0b2c7ef44442c110c15
ZY <zhangyi.201092@gmail.com>
2021-05-05 10:29:14 +0800

    removed backup files

--- ATSProjREST-v2_backup/src/main/java/com/domain/repository/CountryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.domain.repository;
-
-import com.domain.entity.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CountryRepository extends JpaRepository<Country, Integer> {
-}
